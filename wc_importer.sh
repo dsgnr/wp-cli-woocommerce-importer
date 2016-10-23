07882187945947a01bff49788a1902c0096877e1
Dan <email@danielhand.io>
2016-10-23 16:27:36 +0100

    Update wc_importer.sh

--- wc_importer.sh
+++ wc_importer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "I am about to import products!"
 
-while IFS='|' read -r product_title sku regular_price
+while IFS='|' read -r product_title sku regular_price images
 do
-  wp wc product create --title="${product_title}" --regular_price="${regular_price}" --sku="${sku}" --allow-root
+  wp wc product create --title="${product_title}" --regular_price="${regular_price}" --sku="${sku}" --images.0.src="${images}" --allow-root
 done < products.txt
