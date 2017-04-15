2d64a9ce8e856f90187c08e0f78aa4192e226f83
Dan <email@danielhand.io>
2017-04-15 09:54:21 +0100

    Update wc_importer.sh

--- wc_importer.sh
+++ wc_importer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "Grabbing your products now!"
-while IFS='|' read -r product_title sku regular_price images
+while IFS='|' read -r sku product_title description category regular_price weight images
 do
-wp media import "${images}" --featured_image --post_id=$(wp wc product create --title="${product_title}" --regular_price="${regular_price}" --sku="${sku}" --allow-root | awk '{print $4}') --allow-root
-done < products.txt
+wp media import "${images}" --featured_image --post_id=$(wp wc product create --title="${product_title}" --short_description="${description}" --categories="{$category}" --weight="${weight}" --regular_price="${regular_price}" --sku="${sku}" --allow-root | awk '{print $4}') --allow-root
+done < test.txt
